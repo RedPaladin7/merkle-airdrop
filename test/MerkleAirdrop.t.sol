638b582c08b56372a912979f899538d7ed1dd0c2
RedPaladin7 <ishans7325@gmail.com>
2025-06-27 20:06:32 +0200

    implemented signatures

--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -2,8 +2,57 @@
 
 pragma solidity ^0.8.24;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
+import {BagelToken} from "../src/BagelToken.sol";
+import {ZkSyncChainChecker} from "lib/foundry-devops/src/ZkSyncChainChecker.sol";
+import {DeployMerkleAirdrop} from "../script/DeployMerkleAirdrop.s.sol";
 
-contract MerkleAirdropTest is Test {
-    
+contract MerkleAirdropTest is Test, ZkSyncChainChecker {
+    MerkleAirdrop airdrop;
+    BagelToken token;
+
+    bytes32 ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    uint256 AMOUNT_TO_CLAIM = 25 * 1e18;
+    uint256 AMOUNT_TO_SEND = AMOUNT_TO_CLAIM * 4;
+    bytes32 p1 = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 p2 = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] PROOF = [p1, p2];
+    address user;
+    uint256 userPrivKey;
+    address gasPayer;
+
+    function setUp() public {
+        if(!isZkSyncChain()){
+            DeployMerkleAirdrop deployer = new DeployMerkleAirdrop();
+            (token, airdrop) = deployer.deployMerkleAirdrop();
+        } else {
+            token = new BagelToken();
+            airdrop = new MerkleAirdrop(ROOT, token);
+            // the airdrop contract to needs to hold the funds that it plans to give away to the white listed users 
+            token.mint(token.owner(), AMOUNT_TO_SEND);
+            // in testing environment the test contract is msg.sender and hence the owner of the instance of the token we created
+            // in production environment, the contract will called by an actual address
+            token.transfer(address(airdrop), AMOUNT_TO_SEND);
+            // first minting to the owner and then transferring to the aidrop contract, because owner is the main handler of the tokens (also possible to directly mint to the airdrop contract)
+        }
+        (user, userPrivKey) = makeAddrAndKey("user");
+        gasPayer = makeAddr("gasPayer");
+    }
+
+    function testUsersCanClaim() public {
+        uint256 startingBalance = token.balanceOf(user);
+        bytes32 digest = airdrop.getMessageHash(user, AMOUNT_TO_CLAIM);
+
+        // user signs the message
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPrivKey, digest);
+
+        // gasPayers does the claiming
+        vm.prank(gasPayer);
+        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF, v, r, s);
+
+        uint256 endingBalance = token.balanceOf(user);
+        console.log("Ending balance: %d", endingBalance);
+        assertEq(endingBalance - startingBalance, AMOUNT_TO_CLAIM);
+    }
 }
